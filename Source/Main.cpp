01ca5c30298d67df6b36de0a563f0cbd431fb135
Vanja Kostic <vanjakostic03@gmail.com>
2026-01-29 02:34:18 +0100

    [ADD] - initial commit

---
+++ Source/Main.cpp
@@ -0,0 +1,40 @@
+﻿#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "../Header/Util.h"
+
+// Main fajl funkcija sa osnovnim komponentama OpenGL programa
+
+// Projekat je dozvoljeno pisati počevši od ovog kostura
+// Toplo se preporučuje razdvajanje koda po fajlovima (i eventualno potfolderima) !!!
+// Srećan rad!
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 800, "Kostur", NULL, NULL);
+    if (window == NULL) return endProgram("Prozor nije uspeo da se kreira.");
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) return endProgram("GLEW nije uspeo da se inicijalizuje.");
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    glClearColor(0.2f, 0.8f, 0.6f, 1.0f);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
