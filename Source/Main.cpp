19d39d071e3404b8ebefd0003c640d62bc1f2a78
Vanja Kostic <vanjakostic03@gmail.com>
2026-02-02 21:08:38 +0100

    [ADD] - structure

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,40 +1,91 @@
-﻿#include <GL/glew.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include "../header/Util.h"
+
+
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#include "../Header/Util.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
-// Main fajl funkcija sa osnovnim komponentama OpenGL programa
 
-// Projekat je dozvoljeno pisati počevši od ovog kostura
-// Toplo se preporučuje razdvajanje koda po fajlovima (i eventualno potfolderima) !!!
-// Srećan rad!
+
 int main()
 {
-    glfwInit();
+    // ================= INIT GLFW =================
+    if (!glfwInit()) {
+        std::cout << "GLFW nije ucitan!\n";
+        return 1;
+    }
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 800, "Kostur", NULL, NULL);
-    if (window == NULL) return endProgram("Prozor nije uspeo da se kreira.");
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", NULL, NULL);
+    if (!window) {
+        std::cout << "Prozor nije kreiran!\n";
+        glfwTerminate();
+        return 2;
+    }
     glfwMakeContextCurrent(window);
 
-    if (glewInit() != GLEW_OK) return endProgram("GLEW nije uspeo da se inicijalizuje.");
+    if (glewInit() != GLEW_OK) {
+        std::cout << "GLEW nije ucitan!\n";
+        return 3;
+    }
 
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    // ================= BAFERI =================
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
 
-    glClearColor(0.2f, 0.8f, 0.6f, 1.0f);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // glBufferData(...);  // podaci ce ici kasnije
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 
+    // ================= SEJDERI =================
+    unsigned int shaderProgram = createShader("vertex.vert", "fragment.frag");
+
+    // ================= MATRICI =================
+    glm::mat4 model = glm::mat4(1.0f);
+    glm::mat4 view = glm::mat4(1.0f);
+    glm::mat4 projection = glm::mat4(1.0f);
+
+    // ================= RENDER LOOP =================
+    glEnable(GL_DEPTH_TEST);
     while (!glfwWindowShouldClose(window))
     {
-        glClear(GL_COLOR_BUFFER_BIT);
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, true);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        // glDrawArrays(...) ili glDrawElements(...);
+        glBindVertexArray(0);
+        glUseProgram(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    glfwDestroyWindow(window);
+    // ================= POSPREMANJE =================
+    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteProgram(shaderProgram);
+
     glfwTerminate();
     return 0;
 }
+
